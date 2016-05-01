29085bcb6a6b14af262393f20f12f6ac6dcb2fd4
Ma Yanlong <mayanlong7@icloud.com>
2016-05-01 22:53:03 +0800

    init

---
+++ app/scripts/main.js
@@ -0,0 +1,57 @@
+$(function(){
+    $.getJSON('/css.json').done(function(data){
+      var categorys = {};
+      $.each(data, function(_,v){
+        var key = v['spec name'];
+        if ( ! categorys[ key ] ){
+          categorys[ key ] = [];
+          console.log(key);
+        }
+        categorys[ key ].push(v);
+      })
+      $('.main-content').append(function(){
+        return  $.map(order, function( v, i ){
+          var h1 = v.h1,  h2s = v.h2s;
+          var html  =
+          '<article class="theme'+[i%8]+'">' +
+          '<h1>'+ h1 +'</h1>' +
+          '<div>' +
+
+          $.map(h2s, function(h2){
+            return '<div>' +
+
+            $.map(h2, function(v){
+              return '<section>' +
+              '<h2>'+ v +'</h2>' +
+              '<ul>' +
+
+              $.map(categorys[v], function(v){
+                if( v.Name ){
+                  var name =  (v.Name.indexOf(',') !== -1)?( v.Name.split(',')[0] ):v.Name ;
+                }
+                return '<li>' +
+                '<a href="'+v.uri+'" title="'+v.Initial+'" '+
+                'data-apply="'+v['Applies To']+'" '+
+                'data-value="'+v.Value+'" data-initial="'+v.Initial+'">' +
+                name +
+                '</a>' +
+                '</li>';
+              }).join('') +
+
+              '</ul>' +
+              '</section>'
+            }).join('') +
+
+            '</div>';
+          }).join('') +
+
+          '</div>' +
+          '</article>';
+          return html;
+          ///first loop end below!
+        });
+        //// append end below!
+      });
+      ///ajax .done end below!
+    });
+  });
---
+++ app/scripts/order.js
@@ -0,0 +1,145 @@
+/*
+
+"CSS basic box model",
+"CSS Overflow Module Level 3",
+"CSS Backgrounds and Borders Module Level 3",
+"CSS Basic User Interface Module Level 3",
+"CSS Device Adaptation",
+"CSS3 module: line",
+"CSS3 module: Border",
+
+"CSS Fonts Module Level 3",
+"CSS Color Module Level 3",
+"CSS Text Decoration Module Level 3",
+"CSS Text Module Level 3",
+
+
+"CSS Animations",
+"CSS Transitions",
+"CSS Transforms",
+"CSS 2D Transforms",
+"CSS 3D Transforms Module Level 3",
+
+
+"CSS Grid Layout",
+"CSS Flexible Box Layout Module",
+"CSS Template Layout Module",
+"CSS Multi-column Layout Module",
+"CSS Grid Positioning Module Level 3",
+"CSS Box Alignment Module Level 3",
+
+"Filter Effects 1.0",
+
+"CSS Speech Module",
+"CSS Masking Level 1",
+"CSS Exclusions Module Level 1",
+"CSS Regions Module Level 1",
+"CSS Writing Modes Module Level 3",
+"CSS Fragmentation Module Level 3",
+"CSS Generated Content for Paged Media Module",
+"CSS3 Ruby Module",
+"CSS Marquee Module Level 3",
+"CSS Lists and Counters Module Level 3",
+"CSS3 Hyperlink Presentation Module",
+"CSS3 module: Presentation Levels",
+"CSS3 Generated and Replaced Content Module",
+
+"CSS Paged Media Module Level 3",
+"CSS Image Values and Replaced Content Module Level 3",
+"CSS Image Values and Replaced Content Module Level 4",
+"Compositing and Blending Level 1",
+"CSS Intrinsic & Extrinsic Sizing Module Level 3",
+
+*/
+
+var order = [
+  {
+    h1:'CSS basic',
+    h2s:[
+      [
+        "CSS basic box model",
+        "CSS Overflow Module Level 3",
+      ],
+      [
+        "CSS Backgrounds and Borders Module Level 3",
+      ],
+      [
+        "CSS Basic User Interface Module Level 3",
+        "CSS Device Adaptation",
+      ],
+    ]
+  },
+  {
+    h1:'Font',
+    h2s:[
+      [
+        "CSS Fonts Module Level 3",
+        "CSS Color Module Level 3",
+      ],
+      [
+        "CSS Text Module Level 3",
+        "CSS Text Decoration Module Level 3",
+      ],
+    ]
+  },
+  {
+    h1:'CSS Transform Animation',
+    h2s:[
+      [
+        "CSS Animations",
+        "CSS Transitions",
+        "CSS 3D Transforms Module Level 3",
+      ],
+    ]
+  },
+  {
+    h1:'Layout',
+    h2s:[
+      [
+        "CSS Grid Layout",
+        "CSS Flexible Box Layout Module",
+      ],
+      [
+        "CSS Template Layout Module",
+        "CSS3 module: line",
+        "CSS Multi-column Layout Module",
+      ],
+      [
+        "CSS Box Alignment Module Level 3",
+        "CSS Grid Positioning Module Level 3",
+      ],
+    ]
+  },
+  {
+    h1:'Filter',
+    h2s:[
+      [
+        "Filter Effects 1.0",
+      ],
+    ]
+  },
+  {
+    h1:'Other',
+    h2s:[
+      [
+        "CSS Speech Module",
+        "CSS Masking Level 1",
+      ],
+      [
+        "CSS Exclusions Module Level 1",
+        "CSS Regions Module Level 1",
+        "CSS Writing Modes Module Level 3",
+        "CSS Fragmentation Module Level 3",
+        "CSS Generated Content for Paged Media Module",
+        "CSS3 Ruby Module",
+      ],
+      [
+        "CSS Marquee Module Level 3",
+        "CSS Lists and Counters Module Level 3",
+        "CSS3 Hyperlink Presentation Module",
+        "CSS3 module: Presentation Levels",
+        "CSS3 Generated and Replaced Content Module",
+      ],
+    ]
+  },
+];
